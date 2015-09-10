65e5dd9da5fe8678eefb5c04be738a325f2faa75
aylin.capli <aylin.capli@L2121.buro.info.nl>
2015-09-10 17:15:37 +0200

    2 javascript files were committed.

--- src/main/webapp/googleMaps.js
+++ src/main/webapp/googleMaps.js
@@ -3,12 +3,12 @@
 <head>
 <meta charset="ISO-8859-1">
 <title>Insert title here</title>
-<link rel="stylesheet" href="Css/maps.css">
+<link rel="stylesheet" href="css/maps.css">
 <script
 	src="http://ajax.googleapis.com/ajax/libs/angularjs/1.0.3/angular.min.js" type="text/javascript"></script>
 <script
 	src="http://maps.googleapis.com/maps/api/js?sensor=false&language=en"></script>
-<script type="text/javascript" src="Js/maps.js"></script>
+<script type="text/javascript" src="js/maps.js"></script>
 </head>
 <body>
 
